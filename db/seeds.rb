945bb7d279fb78307e66ab97decb8bc07b08d80f
Delfrinando Pranata <delfrinando@gmail.com>
2016-11-17 11:21:19 +0700

    '#'

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+ActiveRecord::Base.connection.tables.each do |table|
+  ActiveRecord::Base.connection.execute("TRUNCATE #{table}")
+end
+
 Drink.create!(title: 'espresso', description: 'This is espresso description')
 Drink.create!(title: 'latte', description: 'This is latte description')
 Drink.create!(title: 'cappucino')
@@ -13,8 +17,8 @@ Drink.create!(title: 'green tea', description: 'This is green tea description')
 Drink.create!(title: 'hot tea', description: 'This is hot tea description')
 
 
-Price.create!(id_drinks: '1', tall: '1.95', tall: '2.05', venti: '2.35')
-Price.create!(id_drinks: '2', tall: '3.4', tall: '4.45', venti: '4.65')
-Price.create!(id_drinks: '3', tall: '3.15', tall: '3.75', venti: '4.15')
-Price.create!(id_drinks: '4', tall: '3.45', tall: '4.25', venti: '4.45')
-Price.create!(id_drinks: '5', tall: '', tall: '2.05', venti: '')
+Price.create!(id_drinks: '1', tall: '1.95', grande: '2.05', venti: '2.35')
+Price.create!(id_drinks: '2', tall: '3.4', grande: '4.45', venti: '4.65')
+Price.create!(id_drinks: '3', tall: '3.15', grande: '3.75', venti: '4.15')
+Price.create!(id_drinks: '4', tall: '3.45', grande: '4.25', venti: '4.45')
+Price.create!(id_drinks: '5', tall: '', grande: '2.05', venti: '')
