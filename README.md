3bd93713103272280e5c93935e72aff142d93696
Delfrinando Pranata <delfrinando@gmail.com>
2016-11-17 11:09:41 +0700

    '#'

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+This Ruby on Rails simple application made by Delfrinando Pranata as requested by wego.com for technical test.
 
-Things you may want to cover:
 
 * Ruby version
-
 ```
 ruby 2.2.4p230 (2015-12-16 revision 53155) [i386-mingw32]
 ```
-
 
 * Deployment instructions
 
@@ -21,3 +17,5 @@ Here is the step that you should do!
     3. Then migrate the default database by using "rake db:seed"
     4. Run the server using "rails s"
 ```
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Drink.create!(title: 'cappucino', description: 'This is cappucino description')
-Drink.create!(title: 'frapuccino', description: 'This is frapuccino description')
-Drink.create!(title: 'latte')
-Drink.create!(title: 'robusta', description: 'This is robusta description')
-Drink.create!(title: 'hot tea', description: 'This is hot tea description')
+Drink.create!(title: 'espresso', description: 'This is espresso description')
+Drink.create!(title: 'latte', description: 'This is latte description')
+Drink.create!(title: 'cappucino')
 Drink.create!(title: 'green tea', description: 'This is green tea description')
+Drink.create!(title: 'hot tea', description: 'This is hot tea description')
+
+
+Price.create!(id_drinks: '1', tall: '1.95', tall: '2.05', venti: '2.35')
+Price.create!(id_drinks: '2', tall: '3.4', tall: '4.45', venti: '4.65')
+Price.create!(id_drinks: '3', tall: '3.15', tall: '3.75', venti: '4.15')
+Price.create!(id_drinks: '4', tall: '3.45', tall: '4.25', venti: '4.45')
+Price.create!(id_drinks: '5', tall: '', tall: '2.05', venti: '')
